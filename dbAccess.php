fcd75e859ecc2c8b338ded42cb76561021cc0420
russell1969Din <rasto@fermata.sk>
2021-03-20 19:42:01 +0100

    Update db Info

--- dbAccess.php
+++ dbAccess.php
@@ -1,7 +1,7 @@
 <?
-    $this->dbLocal  =   "46.229.230.119";
-    $this->dbLogin  =   "op024700";
-    $this->dbName   =   "op024700db";
-    $this->dbPass   =   "pduhufet";
+    $this->dbLocal  =   "...";
+    $this->dbLogin  =   "...";
+    $this->dbName   =   "...";
+    $this->dbPass   =   "...";
     $this->dbPort   =   "";
 ?>
